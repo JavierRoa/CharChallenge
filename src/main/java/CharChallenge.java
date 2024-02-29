b6b2064229fe2d6a2e799aba66264ce497efc4b6
Javier Roa <javierroa159@gmail.com>
2024-02-29 13:05:31 -0300

    Initial commit

---
+++ src/main/java/CharChallenge.java
@@ -0,0 +1,14 @@
+public class CharChallenge {
+    public static void main(String[] args) {
+
+        char mySimpleChar = '?';
+        char myUnicodeChar = '\u003F';
+        char myDecimalChar = (char)63;
+
+        System.out.println("My values are:\n" + mySimpleChar + " for mySimpleChar\n"
+                + myUnicodeChar + " for myUnicodeChar\n"
+                + myDecimalChar + " for myDecimalChar");
+
+
+    }
+}
